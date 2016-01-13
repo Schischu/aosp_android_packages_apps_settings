50b164d246dadb710b0f4e0ca9702b6b90a6f958
Schischu <schischu65@gmail.com>
2016-01-13 11:50:27 -0800

    Add options for screen colortone, wifi country code and button backlight
    Conflicts:
    	src/com/android/settings/DisplaySettings.java
    
    Conflicts:
    	res/values/arrays.xml
    	res/values/strings.xml
    	res/xml/display_settings.xml
    	res/xml/wifi_advanced_settings.xml
    	src/com/android/settings/DisplaySettings.java
    	src/com/android/settings/wifi/AdvancedWifiSettings.java
    
    Change-Id: Ic3de27744ec140764dc822b8f7628c702009f313

--- src/com/android/settings/wifi/AdvancedWifiSettings.java
+++ src/com/android/settings/wifi/AdvancedWifiSettings.java
@@ -36,6 +36,7 @@ import com.android.settingslib.RestrictedLockUtils;
 public class AdvancedWifiSettings extends RestrictedSettingsFragment {
     private static final String TAG = "AdvancedWifiSettings";
 
+    private static final String KEY_COUNTRY_CODE = "country_code";
     private static final String KEY_INSTALL_CREDENTIALS = "install_credentials";
     private static final String KEY_WIFI_DIRECT = "wifi_direct";
     private static final String KEY_WPS_PUSH = "wps_push_button";
@@ -114,6 +115,195 @@ public class AdvancedWifiSettings extends RestrictedSettingsFragment {
                     return true;
                 }
         });
+<<<<<<< HEAD
+=======
+
+        ListPreference frequencyPref = (ListPreference) findPreference(KEY_FREQUENCY_BAND);
+
+        if (mWifiManager.isDualBandSupported()) {
+            frequencyPref.setOnPreferenceChangeListener(this);
+            int value = mWifiManager.getFrequencyBand();
+            if (value != -1) {
+                frequencyPref.setValue(String.valueOf(value));
+                updateFrequencyBandSummary(frequencyPref, value);
+            } else {
+                Log.e(TAG, "Failed to fetch frequency band");
+            }
+        } else {
+            if (frequencyPref != null) {
+                // null if it has already been removed before resume
+                getPreferenceScreen().removePreference(frequencyPref);
+            }
+        }
+
+        ListPreference countryPref = (ListPreference) findPreference(KEY_COUNTRY_CODE);
+        countryPref.setOnPreferenceChangeListener(this);
+        String ccValue = mWifiManager.getCountryCode();
+        if (ccValue != null) {
+            countryPref.setValue(ccValue);
+            updateCountryCodeSummary(countryPref, ccValue);
+        } else {
+            Log.e(TAG, "Failed to fetch country code");
+        }
+
+        ListPreference sleepPolicyPref = (ListPreference) findPreference(KEY_SLEEP_POLICY);
+        if (sleepPolicyPref != null) {
+            if (Utils.isWifiOnly(context)) {
+                sleepPolicyPref.setEntries(R.array.wifi_sleep_policy_entries_wifi_only);
+            }
+            sleepPolicyPref.setOnPreferenceChangeListener(this);
+            int value = Settings.Global.getInt(getContentResolver(),
+                    Settings.Global.WIFI_SLEEP_POLICY,
+                    Settings.Global.WIFI_SLEEP_POLICY_NEVER);
+            String stringValue = String.valueOf(value);
+            sleepPolicyPref.setValue(stringValue);
+            updateSleepPolicySummary(sleepPolicyPref, stringValue);
+        }
+    }
+
+    private void initWifiAssistantPreference(Collection<NetworkScorerAppData> scorers) {
+        int count = scorers.size();
+        String[] packageNames = new String[count];
+        int i = 0;
+        for (NetworkScorerAppData scorer : scorers) {
+            packageNames[i] = scorer.mPackageName;
+            i++;
+        }
+        mWifiAssistantPreference.setPackageNames(packageNames,
+                mNetworkScoreManager.getActiveScorerPackage());
+    }
+
+    private void updateSleepPolicySummary(Preference sleepPolicyPref, String value) {
+        if (value != null) {
+            String[] values = getResources().getStringArray(R.array.wifi_sleep_policy_values);
+            final int summaryArrayResId = Utils.isWifiOnly(getActivity()) ?
+                    R.array.wifi_sleep_policy_entries_wifi_only : R.array.wifi_sleep_policy_entries;
+            String[] summaries = getResources().getStringArray(summaryArrayResId);
+            for (int i = 0; i < values.length; i++) {
+                if (value.equals(values[i])) {
+                    if (i < summaries.length) {
+                        sleepPolicyPref.setSummary(summaries[i]);
+                        return;
+                    }
+                }
+            }
+        }
+
+        sleepPolicyPref.setSummary("");
+        Log.e(TAG, "Invalid sleep policy value: " + value);
+    }
+
+    private void updateFrequencyBandSummary(Preference frequencyBandPref, int index) {
+        String[] summaries = getResources().getStringArray(R.array.wifi_frequency_band_entries);
+        frequencyBandPref.setSummary(summaries[index]);
+    }
+
+    private void updateCountryCodeSummary(Preference countryCodePref, String value) {
+        ListPreference countryCodeListPreference = (ListPreference) countryCodePref;
+        int index = countryCodeListPreference.findIndexOfValue(value);
+        String[] summaries = getResources().getStringArray(R.array.wifi_country_code_entries);
+        countryCodeListPreference.setSummary(summaries[index]);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen screen, Preference preference) {
+        String key = preference.getKey();
+
+        if (KEY_NOTIFY_OPEN_NETWORKS.equals(key)) {
+            Global.putInt(getContentResolver(),
+                    Settings.Global.WIFI_NETWORKS_AVAILABLE_NOTIFICATION_ON,
+                    ((SwitchPreference) preference).isChecked() ? 1 : 0);
+        } else {
+            return super.onPreferenceTreeClick(screen, preference);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final Context context = getActivity();
+        String key = preference.getKey();
+
+        if (KEY_FREQUENCY_BAND.equals(key)) {
+            try {
+                int value = Integer.parseInt((String) newValue);
+                mWifiManager.setFrequencyBand(value, true);
+                updateFrequencyBandSummary(preference, value);
+            } catch (NumberFormatException e) {
+                Toast.makeText(context, R.string.wifi_setting_frequency_band_error,
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else if (KEY_COUNTRY_CODE.equals(key)) {
+            try {
+                String value = (String) newValue;
+                mWifiManager.setCountryCode(value, true);
+                updateCountryCodeSummary(preference, value);
+            } catch (NumberFormatException e) {
+                Toast.makeText(context, R.string.wifi_setting_country_code_error,
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else if (KEY_WIFI_ASSISTANT.equals(key)) {
+            NetworkScorerAppData wifiAssistant =
+                    NetworkScorerAppManager.getScorer(context, (String) newValue);
+            if (wifiAssistant == null) {
+                mNetworkScoreManager.setActiveScorer(null);
+                return true;
+            }
+
+            Intent intent = new Intent();
+            if (wifiAssistant.mConfigurationActivityClassName != null) {
+                // App has a custom configuration activity; launch that.
+                // This custom activity will be responsible for launching the system
+                // dialog.
+                intent.setClassName(wifiAssistant.mPackageName,
+                        wifiAssistant.mConfigurationActivityClassName);
+            } else {
+                // Fall back on the system dialog.
+                intent.setAction(NetworkScoreManager.ACTION_CHANGE_ACTIVE);
+                intent.putExtra(NetworkScoreManager.EXTRA_PACKAGE_NAME,
+                        wifiAssistant.mPackageName);
+            }
+
+            startActivity(intent);
+            // Don't update the preference widget state until the child activity returns.
+            // It will be updated in onResume after the activity finishes.
+            return false;
+        }
+
+        if (KEY_SLEEP_POLICY.equals(key)) {
+            try {
+                String stringValue = (String) newValue;
+                Settings.Global.putInt(getContentResolver(), Settings.Global.WIFI_SLEEP_POLICY,
+                        Integer.parseInt(stringValue));
+                updateSleepPolicySummary(preference, stringValue);
+            } catch (NumberFormatException e) {
+                Toast.makeText(context, R.string.wifi_setting_sleep_policy_error,
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void refreshWifiInfo() {
+        final Context context = getActivity();
+        WifiInfo wifiInfo = mWifiManager.getConnectionInfo();
+
+        Preference wifiMacAddressPref = findPreference(KEY_MAC_ADDRESS);
+        String macAddress = wifiInfo == null ? null : wifiInfo.getMacAddress();
+        wifiMacAddressPref.setSummary(!TextUtils.isEmpty(macAddress) ? macAddress
+                : context.getString(R.string.status_unavailable));
+        wifiMacAddressPref.setSelectable(false);
+
+        Preference wifiIpAddressPref = findPreference(KEY_CURRENT_IP_ADDRESS);
+        String ipAddress = Utils.getWifiIpAddresses(context);
+        wifiIpAddressPref.setSummary(ipAddress == null ?
+                context.getString(R.string.status_unavailable) : ipAddress);
+        wifiIpAddressPref.setSelectable(false);
+>>>>>>> 24e3558... Add options for screen colortone, wifi country code and button backlight
     }
 
     /* Wrapper class for the WPS dialog to properly handle life cycle events like rotation. */
