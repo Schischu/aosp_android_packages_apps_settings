df3950b226ae5275da0bc9b400b860f0de701edc
Schischu <schischu65@gmail.com>
2016-01-13 11:51:11 -0800

    Always show the multiwindow option

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -817,7 +817,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     }
 
     private static boolean showEnableMultiWindowPreference() {
-        return !"user".equals(Build.TYPE);
+        return true; //!"user".equals(Build.TYPE);
     }
 
     private void setEnableMultiWindow(boolean value) {
